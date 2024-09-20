cff78307d8f14fa8899997307f5b6baf65a91e3f
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-09-20 23:08:04 +0530

    🏷️ | remove LocalAccount type

--- snippets/python/main.py
+++ snippets/python/main.py
@@ -1,8 +1,6 @@
 import json
 from web3 import Web3
 from eth_account import Account
-from eth_account.signers.local import LocalAccount
-
 
 def main():
     w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
@@ -10,7 +8,7 @@ def main():
     with open("../../common/contracts/Storage.abi", "r", -1, "utf-8") as f:
         abi = json.load(f)
 
-    account: LocalAccount = Account.from_key(
+    account = Account.from_key(
         "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
     )
 
